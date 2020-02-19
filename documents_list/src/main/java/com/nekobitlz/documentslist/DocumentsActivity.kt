5e41e22dd3317eaa4cfb10d54788e16ac70c5b21
Nekobitlz <jokers.bit@gmail.com>
2020-02-20 00:17:28 +0300

    Documents_list: Initial commit

---
+++ documents_list/src/main/java/com/nekobitlz/documentslist/DocumentsActivity.kt
@@ -0,0 +1,12 @@
+package com.nekobitlz.documentslist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DocumentsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_documents)
+    }
+}
