23ceef55584d641b722b6ed862c5cd3044f06cd9
Nekobitlz <jokers.bit@gmail.com>
2020-02-20 01:32:15 +0300

    Add document fragment to main activity

--- documents_list/src/main/java/com/nekobitlz/documentslist/DocumentsActivity.kt
+++ documents_list/src/main/java/com/nekobitlz/documentslist/DocumentsActivity.kt
@@ -2,11 +2,19 @@ package com.nekobitlz.documentslist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nekobitlz.documentslist.features.DocumentsFragment
 
 class DocumentsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_documents)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, DocumentsFragment())
+                .commit()
+        }
     }
 }
---
+++ documents_list/src/main/java/com/nekobitlz/documentslist/features/DocumentsFragment.kt
@@ -0,0 +1,7 @@
+package com.nekobitlz.documentslist.features
+
+import androidx.fragment.app.Fragment
+
+class DocumentsFragment : Fragment() {
+
+}
