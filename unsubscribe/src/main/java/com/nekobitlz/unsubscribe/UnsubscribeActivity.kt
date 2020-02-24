a1ceb23c93facabdd3117614d993e8dd9d5abdc5
Nekobitlz <jokers.bit@gmail.com>
2020-02-25 01:46:38 +0300

    Unsubscribe app: Initial commit

---
+++ unsubscribe/src/main/java/com/nekobitlz/unsubscribe/UnsubscribeActivity.kt
@@ -0,0 +1,12 @@
+package com.nekobitlz.unsubscribe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UnsubscribeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unsubscribe)
+    }
+}
