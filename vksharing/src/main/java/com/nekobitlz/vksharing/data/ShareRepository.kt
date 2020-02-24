a4fc897c8035032ee5b5f7124338deb7b298062e
Nekobitlz <jokers.bit@gmail.com>
2020-02-24 16:29:23 +0300

    Implement repository for VK posting

---
+++ vksharing/src/main/java/com/nekobitlz/vksharing/data/ShareRepository.kt
@@ -0,0 +1,35 @@
+package com.nekobitlz.vksharing.data
+
+import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.nekobitlz.vksharing.data.commands.VKWallPostCommand
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.VKApiCallback
+
+interface IShareRepository {
+    fun sendPost(userId: Int = 0, message: String, attachment: Uri?): LiveData<Int>
+}
+
+class ShareRepository : IShareRepository {
+
+    override fun sendPost(userId: Int, message: String, attachment: Uri?): LiveData<Int> {
+        val command = VKWallPostCommand(message = message, photos = attachment, ownerId = userId)
+        val data = MutableLiveData<Int>()
+        Log.e("DEBUG", "Send repos $data ${data.hasObservers()}")
+
+        VK.execute(command, object : VKApiCallback<Int> {
+            override fun success(result: Int) {
+                data.value = result
+            }
+
+            override fun fail(error: Exception) {
+                error.printStackTrace()
+                data.value = -1
+            }
+        })
+
+        return data
+    }
+}
