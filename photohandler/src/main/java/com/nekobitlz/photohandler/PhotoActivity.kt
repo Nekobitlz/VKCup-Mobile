21b9dc4762e2a6e7376b615f38aa7b87eeb0c89e
Nekobitlz <jokers.bit@gmail.com>
2020-03-01 16:31:56 +0300

    Photo handler: Initial commit

---
+++ photohandler/src/main/java/com/nekobitlz/photohandler/PhotoActivity.kt
@@ -0,0 +1,12 @@
+package com.nekobitlz.photohandler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo)
+    }
+}
